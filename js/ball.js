930fa47d8aa88d7430ac94dda065b9a4d4f826fa
Javier Samper <jsamperarias@hotmail.com>
2018-11-05 19:24:16 +0100

    Estructura básica:rebotes,movimiento paletas y pelota

--- js/ball.js
+++ js/ball.js
@@ -1,20 +1,37 @@
 function Ball(x, y, vx, vy, radius, color) {
+    this.canvas = this.canvas;
     this.ctx = canvas.getContext("2d");
     this.x = x;
     this.y = y;
-    this.vx = vx;
-    this.vy = vy;
+    this.vx = 5;
+    this.vy = 5;
     this.radius = radius;
     this.color = color;
-
-
+    this.width = this.with;
+    this.height = this.height;
 
 }
+//borramos rastro pelota
+Ball.prototype.clear = function() {
 
+
+    //this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}
+
+//dibujamos pelota
 Ball.prototype.draw = function() {
-    this.ctx.beginPath();
-    this.ctx.fillStyle = "white";
-    this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    this.ctx.fill();
-    this.ctx.closePath();
+        this.ctx.beginPath();
+        this.ctx.fillStyle = "white";
+        this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
+    //movimiento pelota
+Ball.prototype.move = function() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+
 }
--- js/game.js
+++ js/game.js
@@ -1,27 +1,50 @@
+var KEY_UP = 38;
+var KEY_DOWN = 40;
+var UP_KEY = 87;
+var DOWN_KEY = 83;
+
 function Game() {
-    var canvas = document.getElementById("canvas");
+    this.canvas = document.getElementById("canvas");
     this.ctx = canvas.getContext("2d");
     this.width = canvas.width;
     this.height = canvas.height;
+    this.fps = 60;
 
     this.ctx.fillStyle = "white";
 
-    this.player1 = new Player(20, this.height / 2);
-    this.player2 = new Player(this.width - 20, this.height / 2);
+    this.player1 = new Player(20, this.height / 2.3, 0, 80);
+    this.player2 = new Player(this.width - 20, this.height / 2.3, 0, 80);
+    this.player1.y = this.player1.vy;
+
 
     this.ball = new Ball(100, 100, 0, 0, 2 * Math.PI);
     this.ball.x = this.width / 2;
     this.ball.y = this.height / 2;
 
+    this.ball.x += this.ball.vx;
+    this.ball.y += this.ball.vy;
 
+    this.setListeners();
+}
 
+Game.prototype.clear = function() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}
 
+Game.prototype.move = function() {
+    this.ball.move();
+    if (this.ball.x >= this.width || this.ball.x - this.ball.radius < 0) {
+        this.ball.vx *= -1;
+    }
+
+    if (this.ball.y >= this.height || this.ball.y - this.ball.radius < 0) {
+        this.ball.vy *= -1;
+    }
 }
 
 Game.prototype.draw = function() {
 
     this.ctx.beginPath();
-
 
     //jugadores
     this.player1.draw();
@@ -33,10 +56,37 @@ Game.prototype.draw = function() {
     this.ctx.closePath();
 }
 
+Game.prototype.setListeners = function() {
+    document.onkeydown = function(event) {
+        event.preventDefault();
+        switch (event.keyCode) {
+            case KEY_UP:
+                this.player1.y -= this.player1.vy;
+                break;
+
+            case KEY_DOWN:
+                this.player1.y += this.player1.vy;
+                break;
+            case UP_KEY:
+                this.player2.y -= this.player2.vy;
+                break;
+            case DOWN_KEY:
+                this.player2.y += this.player2.vy;
+                break;
+            default:
+                break;
+        }
+    }.bind(this)
+}
 
 
 
 Game.prototype.start = function() {
-    this.draw();
-    this.update();
+    setInterval(function() {
+        this.setListeners();
+        this.clear();
+        this.move();
+        this.draw();
+
+    }.bind(this), 1000 / this.fps)
 }
